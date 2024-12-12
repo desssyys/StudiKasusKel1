3dca6a28ce1dd73da5b1746dbd39038cddb1ae37
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-12-12 16:15:06 +0700

    tes2

--- SK2_Cafe.java
+++ SK2_Cafe.java
@@ -3,5 +3,6 @@ public class SK2_Cafe {
     public static void main(String[] args) {
         
         System.out.println("bbb");
+        System.out.println("semoga kami sukses bahagia aman sejahterA hala uangnya haji mabrur ");
     }
 }
