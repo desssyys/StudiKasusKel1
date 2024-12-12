518bd8d899f35c166642dffb60155418d576cadb
MeutiaArafah <meutiaarafah@gmail.com>
2024-12-12 16:16:13 +0700

    wkwk

--- SK2_Cafe.java
+++ SK2_Cafe.java
@@ -4,5 +4,6 @@ public class SK2_Cafe {
         
         System.out.println("bbb");
         System.out.println("semoga kami sukses bahagia aman sejahterA hala uangnya haji mabrur ");
+        System.out.println("aamiin");
     }
 }
